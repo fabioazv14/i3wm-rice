f6de8b3dbe5bd1de0c7463c57783d3d090dbf415
fabioazv14 <fabio.azv14@gmail.com>
2025-09-29 23:43:43 +0100

    first setup

---
+++ polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Mata qualquer Polybar que esteja a correr
+killall -q polybar
+
+# Espera até fechar totalmente
+while pgrep -x polybar >/dev/null; do sleep 0.1; done
+
+# Inicia a barra
+polybar mybar &
